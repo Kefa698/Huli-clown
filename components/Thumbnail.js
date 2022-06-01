2eecc74fe6c6de69cb838276af24afed13474c7e
Kefa698 <kefaombongi07@gmail.com>
2022-06-01 19:01:06 +0300

    error
    Signed-off-by: Kefa698 <kefaombongi07@gmail.com>

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,11 +3,11 @@ import Image from 'next/image';
 import { ThumbUpIcon } from '@heroicons/react/outline';
 import { forwardRef } from 'react';
 // eslint-disable-next-line react/display-name
-const Thumbnail = forwardRef(({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
 	const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 	return (
 		<div
-			
+			ref={ref}
 			className="p-2 transition duration-100 ease-in transform cursor-pointer group sm:hover:scale-105 hover:z-50"
 		>
 			<Image
